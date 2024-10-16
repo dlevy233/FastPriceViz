e84e21bb75b1f49a4d56fe8ba79b106477f1e4bc
David Levy <davidlevy@Davids-MacBook-Air.local>
2024-10-16 08:30:27 -0400

    fixed y axis

--- examples/example_market_data.py
+++ examples/example_market_data.py
@@ -3,44 +3,35 @@ from visualization.line_plot import LinePlot
 from api.user_interface import Figure
 from bokeh.models import ColumnDataSource
 
-# Initialize Data Sources
-aapl_data = MarketData(
-    stock_name='AAPL',
-    datetime_range=(
-        '2024-05-01 09:30:00-04:00',
-        '2024-05-01 09:40:00-04:00'
-    ),
-    columns=['price']
-)
-aapl_data.load_data()
-aapl_data.downsample(rule='1s')
+def create_market_data(stock_name, start_time, end_time):
+    data = MarketData(
+        stock_name=stock_name,
+        datetime_range=(start_time, end_time),
+        columns=['price']
+    )
+    data.load_data()
+    data.downsample(rule='1s')
+    return ColumnDataSource(data.data.compute())
 
-googl_data = MarketData(
-    stock_name='GOOGL',
-    datetime_range=(
-        '2024-05-01 09:30:00-04:00',
-        '2024-05-01 09:40:00-04:00'
-    ),
-    columns=['price']
-)
-googl_data.load_data()
-googl_data.downsample(rule='1s')
+def main():
+    # Initialize Data Sources
+    start_time = '2024-05-01 09:30:00-04:00'
+    end_time = '2024-05-01 09:40:00-04:00'
 
-# Create Plot
-plot = LinePlot(title='Stock Prices Comparison', x_axis_label='Time', y_axis_label='Price (AAPL)')
+    aapl_source = create_market_data('AAPL', start_time, end_time)
+    googl_source = create_market_data('GOOGL', start_time, end_time)
 
-# Compute the Dask DataFrames and create ColumnDataSources
-aapl_source = ColumnDataSource(aapl_data.data.compute())
-googl_source = ColumnDataSource(googl_data.data.compute())
+    # Create Plot
+    plot = LinePlot(title='Stock Prices Comparison', x_axis_label='Time', y_axis_label='Price')
 
-# Add multiple lines to the plot with separate y-axes
-plot.add_line(aapl_source, x_axis='timestamp', y_axis='price', color='blue', legend_label='AAPL')
-plot.add_line(googl_source, x_axis='timestamp', y_axis='price', color='red', legend_label='GOOGL', y_range_name='googl')
+    # Add multiple lines to the plot with separate y-axes
+    plot.add_line(aapl_source, x_axis='timestamp', y_axis='price', color='blue', legend_label='AAPL')
+    plot.add_line(googl_source, x_axis='timestamp', y_axis='price', color='red', legend_label='GOOGL', y_range_name='googl')
 
-# Configure the legend
-plot.plot.legend.location = "top_left"
+    # Display Figure
+    fig = Figure()
+    fig.add_plot(plot)
+    fig.show()
 
-# Display Figure
-fig = Figure()
-fig.add_plot(plot)
-fig.show()
+if __name__ == "__main__":
+    main()
--- visualization/line_plot.py
+++ visualization/line_plot.py
@@ -1,6 +1,6 @@
 from visualization.plot_base import PlotBase
 from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, LinearAxis, Range1d
+from bokeh.models import ColumnDataSource, LinearAxis, Range1d, Legend, LegendItem
 
 class LinePlot(PlotBase):
     def __init__(self, title='', x_axis_label='X-Axis', y_axis_label='Y-Axis'):
@@ -8,32 +8,34 @@ class LinePlot(PlotBase):
         self.extra_y_ranges = {}
         self.y_range_names = []
         self.plot = figure(title=title, x_axis_label=x_axis_label, y_axis_label=y_axis_label)
+        self.legend = Legend(click_policy="hide", location="top_left")
+        self.plot.add_layout(self.legend)
 
     def add_line(self, source, x_axis, y_axis, color, legend_label, y_range_name=None):
-        if y_range_name:
-            if y_range_name not in self.extra_y_ranges:
+        y_range_name = y_range_name or 'default'
+
+        if y_range_name not in self.y_range_names:
+            self.y_range_names.append(y_range_name)
+            if y_range_name != 'default':
                 new_range = Range1d()
                 self.extra_y_ranges[y_range_name] = new_range
                 self.plot.extra_y_ranges[y_range_name] = new_range
-                new_axis = LinearAxis(y_range_name=y_range_name, axis_label=legend_label)
+                new_axis = LinearAxis(y_range_name=y_range_name)
                 self.plot.add_layout(new_axis, 'right')
-            y_range_name_to_use = y_range_name
-        else:
-            y_range_name_to_use = 'default'
-
-        if y_range_name_to_use not in self.y_range_names:
-            self.y_range_names.append(y_range_name_to_use)
 
         line = self.plot.line(
             x=x_axis,
             y=y_axis,
             source=source,
             color=color,
-            legend_label=legend_label,
-            y_range_name=y_range_name_to_use
+            y_range_name=y_range_name
         )
         
-        self._update_y_range(source, y_axis, y_range_name_to_use)
+        # Create a LegendItem instead of a tuple
+        legend_item = LegendItem(label=legend_label, renderers=[line])
+        self.legend.items.append(legend_item)
+        
+        self._update_y_range(source, y_axis, y_range_name)
         
         return line
 
@@ -42,18 +44,11 @@ class LinePlot(PlotBase):
         y_min, y_max = min(y_values), max(y_values)
         padding = (y_max - y_min) * 0.1
         
-        if y_range_name == 'default':
-            self.plot.y_range.start = y_min - padding
-            self.plot.y_range.end = y_max + padding
-        else:
-            self.extra_y_ranges[y_range_name].start = y_min - padding
-            self.extra_y_ranges[y_range_name].end = y_max + padding
+        range_to_update = self.plot.y_range if y_range_name == 'default' else self.extra_y_ranges[y_range_name]
+        range_to_update.start = y_min - padding
+        range_to_update.end = y_max + padding
 
     def render(self):
-        # Ensure all y-ranges are properly set
-        for y_range_name in self.y_range_names:
-            if y_range_name != 'default':
-                self.plot.extra_y_ranges[y_range_name] = self.extra_y_ranges[y_range_name]
-
-        self.plot.legend.click_policy = "hide"  # Allow toggling lines on/off
+        for y_range_name, y_range in self.extra_y_ranges.items():
+            self.plot.extra_y_ranges[y_range_name] = y_range
         return self.plot
